a5d9067e7bbf3e4cd3f637343bd0336900817955
Taha <taha_mcp@protonmail.com>
2021-08-23 01:00:01 +0200

    Added automatic highlighting to the helper app

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -9,7 +9,7 @@ var HelpMessageArray = [
     ["-", "Hello! This is the helper window. In this window you will see contextual, helpful messages when you enter specific commands. </br> You can also use the Back and Next buttons to go throughout all the available short help messages."],
     ["-", "At the moment, there are no actual help commands implemented. For help visit the <a href='https://taha-mcp.github.io/terminal-wiki/'>Wiki</a>"],
     ["-", "You can contribute to this project and add short and helpful commands to this helper window. For more information, visit the <a href='https://taha-mcp.github.io/terminal-wiki/Contribution%20Guide'>contribution guide</a>."],
-    ["su", "Here, you are able to use the <i>su</i> command freely. But on a real machine, you most likely need to enter the target user’s password before you are able to log in as them."]
+    ["su", "Here, you are able to use the su command freely. But on a real machine, you most likely need to enter the target user’s password before you are able to log in as them."]
 ];
 
 function NextHelp() {
@@ -33,7 +33,7 @@ function UpdateHelper(Command) {
     }
     else {
         var HelpText = Search(HelpMessageArray, Command, "first", "second").toString();
-        HelperLogs.innerHTML = HelpText;
+        HelperLogs.innerHTML = HelpText.replace(Command, "<span style='color:#FF0000; font-weight:bold;'>" + Command + "</span>");
         HelpIndex = HelpMessageArray.findIndex(([keyword, HelpMessage]) => keyword === Command && HelpMessage === HelpText);
     }
 }
